3a93d2905d365e09b5a89ef03b677090c162df0e
552301 <jinmingchow@gmail.com>
2024-10-31 00:00:18 +0800

    删除无用配置以及修复flyway目录

--- xuankun-cloud-workorder/src/main/java/com/xuankun/workorder/WorkOrderApplication.java
+++ xuankun-cloud-workorder/src/main/java/com/xuankun/workorder/WorkOrderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @date 2022/9/15:9:23
  */
 @EnableTransactionManagement
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WorkOrderApplication extends SpringBootServletInitializer {
 
