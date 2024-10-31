04e57ddf78bec94b66e64e74fd85668af4459296
552301 <jinmingchow@gmail.com>
2024-11-01 00:04:17 +0800

    添加供应链模块

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## 项目说明
 - xuankun-cloud 是采用Spring Cloud Alibaba、SpringSecurity、Spring Cloud Gateway、SpringBoot、Nacos、Redis、Mybatis-Plus等框架，开发的一套SpringCloud快速开发系统，使用门槛极低，且采用MIT开源协议，完全免费开源，可免费用于**商业项目**等场景。
 
+## 部署说明
+- 启动nacos
+- 导入数据库脚本
+- 启动xuankun-cloud-gateway
+- 启动xuankun-cloud-system
+- 启动其他项目
---
+++ xuankun-cloud-scm/src/main/java/com/xuankun/scm/ScmApplication.java
@@ -0,0 +1,15 @@
+package com.xuankun.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ScmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScmApplication.class, args);
+    }
+}
