6d664ede9e804e93f6b0c08e7f9a0adc92618786
552301 <jinmingchow@gmail.com>
2024-11-03 08:52:49 +0800

    添加开源协议

---
+++ xuankun-cloud-scm/src/main/java/com/xuankun/scm/entity/DsBrandEntity.java
@@ -0,0 +1,34 @@
+package com.xuankun.scm.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.xuankun.framework.mybatis.entity.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(value = "品牌表",description = "")
+@EqualsAndHashCode(callSuper = false)
+@TableName("LE_ds_brand")
+public class DsBrandEntity extends BaseEntity implements Serializable,Cloneable{
+
+    /** 品牌代码 */
+    @ApiModelProperty(name = "品牌代码",notes = "")
+    private String brandCode ;
+    /** 品牌名称 */
+    @ApiModelProperty(name = "品牌名称",notes = "")
+    private String brandName ;
+    /** 品牌logo图片地址 */
+    @ApiModelProperty(name = "品牌logo图片地址",notes = "")
+    private String brandLogo ;
+    /** 品牌描述 */
+    @ApiModelProperty(name = "品牌描述",notes = "")
+    private String brandDesc ;
+    /** 排序 */
+    @ApiModelProperty(name = "排序",notes = "")
+    private Integer sort ;
+}
